1eb4281f546b863321b12430cb4b9043162ea6cf
Sam <samkhorrami84@gmail.com>
2026-07-29 15:30:51 +0330

    Request Payment For Getting Zibal TrackId Service Added

--- src/zibal-payment/zibal-payment.module.ts
+++ src/zibal-payment/zibal-payment.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ZibalPaymentService } from './zibal-payment.service';
 
 @Module({
-  providers: [ZibalPaymentService]
+  providers: [ZibalPaymentService],
+  exports: [ZibalPaymentService]
 })
 export class ZibalPaymentModule {}
--- src/zibal-payment/zibal-payment.service.ts
+++ src/zibal-payment/zibal-payment.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import axios from "axios";
 
 @Injectable()
-export class ZibalPaymentService {}
+export class ZibalPaymentService {
+
+    private basePaymenyUrl: string;
+    private readonly merchant: string;
+    private readonly callbackUrl: string;
+
+    constructor () {
+
+        this.merchant = "zibal";
+        this.callbackUrl = "http://localhost:3000";
+        this.basePaymenyUrl = "https://gateway.zibal.ir";
+
+    }
+
+    async requestPayment (amount: number) {
+
+        const requestUrl = `${this.basePaymenyUrl}/v1/request`;
+        const payload = { merchant: this.merchant, amount: (amount * 10), callbackUrl: this.callbackUrl }
+
+        const request = await axios.post(requestUrl, payload, { headers: { "Content-Type": "application/json" } });
+        if (request.data.result !== 100) {
+
+            console.log(request.data);
+            throw new BadRequestException("Something Went wrong while payment");
+
+        }
+
+        return request.data;
+
+    }
+
+}
