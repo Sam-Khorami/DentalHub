f22c0b110d6a6d380ffdb94c1759a0104887f839
Sam <samkhorrami84@gmail.com>
2026-07-31 11:53:07 +0330

    Basic Structure For Likes Is Ready

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { LikesService } from './likes.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../guards/jwtAuth.guard';
 
+@ApiTags("Likes Management")
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('likes')
 export class LikesController {
+  
   constructor(private readonly likesService: LikesService) {}
+
+
 }
--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from '../entity/user.entity';
+import { Product } from '../entity/product.entity';
+import { Comments } from '../entity/comments.entity';
+import { CommentLikes } from '../entity/commentLikes.entity';
 
 @Module({
+  imports: [
+
+    TypeOrmModule.forFeature([User, Product, Comments, CommentLikes])
+
+  ],
   controllers: [LikesController],
   providers: [LikesService],
 })
