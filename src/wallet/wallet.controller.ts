269d33c3b26183655ef427d4dfc55578a27183ec
Sam <samkhorrami84@gmail.com>
2026-08-04 19:59:24 +0330

    Verifing Increase Balance Service Added

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../guards/jwtAuth.guard';
@@ -20,11 +20,19 @@ export class WalletController {
 
   }
 
-  @Post("start-Increase-balance")
+  @Post("start-increase-balance")
   async startIncreaseBalance (@Req() request: Request, @Body() data: IncreaseBalanceDto) {
 
     const paymentData = await this.walletService.startIncreaseBalance(request, data);
     return { data, paymentUrl: `https://gateway.zibal.ir/start/${paymentData.trackId}` }
+
+  }
+
+  @Post("verify-increase-balance/:trackId")
+  async verifyIncreaseBalance (@Req() request: Request, @Param("trackId", ParseIntPipe) trackId: number) {
+
+    await this.walletService.verifyIncreaseBalance(request, trackId);
+    return { message: "Increasing balance is done" }
 
   }
 
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -3,9 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
 import { Repository } from 'typeorm';
 import { Wallet } from '../entity/wallet.entity';
-import { WalletTransaction } from '../entity/walletTransaction.entity';
+import { TransactionType, WalletTransaction } from '../entity/walletTransaction.entity';
 import { IncreaseBalanceDto } from './dto/IncreaseBalance.dto';
 import { ZibalPaymentService } from '../zibal-payment/zibal-payment.service';
+import { CurrencyEnum } from '../enums/entity.enums';
 
 @Injectable()
 export class WalletService {
@@ -48,6 +49,47 @@ export class WalletService {
         // Sending Request To Zibal Service
         const paymentData = await this.zibalPaymentService.requestPayment(data.amount);
         return paymentData; 
+
+    }
+
+    async verifyIncreaseBalance (request: Request, trackId: number) {
+
+        // Getting User Info
+        const userId = request["user"].userId;
+        const user = await this.userRepo.findOne({ where: { id: userId } });
+        if (!user) throw new NotFoundException("User Not Found!");
+
+        //Checking Wallet
+        const wallet = await this.walletRepo.findOne({ where: { user: { id: userId } } });
+        if (!wallet) throw new NotFoundException("Wallet Not Found!");
+
+        // Sending Request To Zibal For Verify Payment
+        const result = await this.zibalPaymentService.verifyPayment(trackId);
+
+        let balanceBefore: number = 0;
+        let balanceAfter: number = 0;
+
+        if (wallet.currency === CurrencyEnum.IRR) {
+            
+            balanceBefore += Number(wallet.balance);
+            balanceAfter += Number(wallet.balance) + result.amount;
+            
+        } 
+        
+        else {
+            
+            balanceBefore += Number(wallet.balance);
+            balanceAfter += Number(wallet.balance) + (result.amount / 10);
+            
+        }
+        
+        const newTransaction = this.transactionsRepo.create({ amount: (result.amount / 10), balanceBefore, balanceAfter, type: TransactionType.Deposit, wallet });
+        await this.transactionsRepo.save(newTransaction);
+        
+        wallet.balance = balanceAfter;
+        await this.walletRepo.save(wallet);
+
+        return;
 
     }
 
