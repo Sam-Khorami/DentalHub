45785bf5f8e788365b1f3b39feec38e680096657
Sam <samkhorrami84@gmail.com>
2026-08-04 17:06:30 +0330

    Entities Added To Wallet Module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entity/user.entity';
+import { Wallet } from '../entity/wallet.entity';
+import { WalletTransaction } from '../entity/walletTransaction.entity';
 
 @Module({
+  imports: [
+
+    TypeOrmModule.forFeature([User, Wallet, WalletTransaction])
+
+  ],
   controllers: [WalletController],
   providers: [WalletService],
 })
