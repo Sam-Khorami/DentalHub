ac0b32227c84b1b7f0c1f521735bf1214314506f
Sam <samkhorrami84@gmail.com>
2026-08-04 17:10:48 +0330

    Structure For Wallet Service Is Ready

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entity/user.entity';
+import { Repository } from 'typeorm';
+import { Wallet } from '../entity/wallet.entity';
+import { WalletTransaction } from '../entity/walletTransaction.entity';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        @InjectRepository(Wallet) private readonly walletRepo: Repository<Wallet>,
+        @InjectRepository(WalletTransaction) private readonly transactionsRepo: Repository<WalletTransaction>
+
+    ) {}
+
+}
