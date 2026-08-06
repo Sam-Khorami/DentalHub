1840184520e82ddddb4d97bd16c6194adbcb0540
Sam <samkhorrami84@gmail.com>
2026-08-06 23:57:31 +0330

    Adding Orders & Product Entities And Redis Module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -6,12 +6,16 @@ import { User } from '../entity/user.entity';
 import { Wallet } from '../entity/wallet.entity';
 import { WalletTransaction } from '../entity/walletTransaction.entity';
 import { ZibalPaymentModule } from '../zibal-payment/zibal-payment.module';
+import { Orders } from '../entity/order.entity';
+import { RedisModule } from '../redis/redis.module';
+import { Product } from '../entity/product.entity';
 
 @Module({
   imports: [
 
-    TypeOrmModule.forFeature([User, Wallet, WalletTransaction]),
-    ZibalPaymentModule
+    TypeOrmModule.forFeature([User, Wallet, WalletTransaction, Orders, Product]),
+    ZibalPaymentModule,
+    RedisModule
 
   ],
   controllers: [WalletController],
